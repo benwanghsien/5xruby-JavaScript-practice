00dbf27432710100c403e91fb7dc09da4616283c
Tgo Wang <ben2021345@hotmail.com>
2022-03-19 22:40:39 +0800

    TODO App finish

--- 09-TODOApp/app.js
+++ 09-TODOApp/app.js
@@ -1,1 +1,34 @@
-// 程式碼寫這裡
+const addBtn = document.querySelector("#addBtn");
+const taskInput = document.querySelector("#taskInput");
+const ul = document.querySelector(".todo-list");
+
+// addbtn click handler
+addBtn.addEventListener("click", () => {
+  renderNewList();
+});
+
+//input enter handler
+taskInput.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    renderNewList();
+  }
+});
+
+// render newList
+function renderNewList() {
+  if (taskInput.value && taskInput.value.trim() !== "") {
+    const newList = `
+        <li class="todo-item">
+          <span class="item">${taskInput.value}</span>
+          <button class="closeBtn">X</button>
+        </li>
+      `;
+
+    ul.insertAdjacentHTML("beforeend", newList);
+
+    const closeBtn = document.querySelector(".closeBtn");
+
+    taskInput.value = "";
+    taskInput.focus();
+  }
+}
